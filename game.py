4cb5c570eb5d44693ab9b44b44594b48de3dbaf4
Sylwester Brzęczkowski <fred@MacBook-Pro-Sylwester.local>
2013-11-02 10:13:30 -0400

    Little aircraft changes, and translator help

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
-§#!/usr/bin/env python3.3
+#!/usr/bin/env python3.3
 
 import curses
 import Pyro4
 
-hardcoded_fire = """
+hardcoded_fire = u"""
  (                     )  (    (                )       (    (   (
  )\ )       *   )   ( /(  )\ ) )\ )    *   ) ( /(       )\ ) )\ ))\ )
 (()/(   ( ` )  /(   )\())(()/((()/(  ` )  /( )\())(    (()/((()/(()/((
@@ -12,6 +12,20 @@ hardcoded_fire = """
 | _ \ | | |_   _|   / _ \| |_ | |_   |_   _|| || | __| | |_ |_ _| _ \ __|
 |  _/ |_| | | |    | (_) | __|| __|    | |  | __ | _|  | __| | ||   / _|
 |_|  \___/  |_|     \___/|_|  |_|      |_|  |_||_|___| |_|  |___|_|_\___|
+ZMIENIC NA PUT OUT THE FIRE - od gościa z samolotu.
 """
 
 print(hardcoded_fire)
+
+class Player:
+    """
+    The player
+    """
+    def __init__(self, name, room, items = []):
+        self.name = name
+        self.room = room
+        self.items = items
+
+    def drop(self, index):
+        self.items.pop(index)
+
--- server.py
+++ server.py
@@ -1,45 +1,62 @@
 #!/usr/bin/env python3.3
-from utils import initializer
 from yaml import safe_load
-from networkx import connected_watts_strogatz_graph
+from networkx import connected_watts_strogatz_graph as graph_gen
 import Pyro4
 import random
 
-class Gamer:
-    """
-    The player
-    """
-    @initializer
-    def __init__(self, name, room, items = []):
-        """
-        Da init
-        """
-        self.items = items
-    def pick_up(self, item):
-        self.items.append(item)
-
-    def drop(self, index):
-        self.items.pop(index)
-
-
 class Room:
     """
-    The room is on fire(maybe)
+    The room is on fire (maybe)
     """
-    @initializer
-    def __init__(self, name, description, items = []):
-        """
-        Da init2
-        """
+    def __init__(self, name, description, items = [], water_source=False):
+        self.name = name
+        self.description = description
         self.items = items
+        self.water_source = water_source
+        self.players = []
+        
 
 class Item:
     """
-    For example bucket, cup or sandwich
+    Item class
     """
-    @initializer
     def __init__(self, name, description, capacity=None):
+        self.name = name
+        self.description = description
         self.capacity = capacity
+
+    def __repr__(self):
+        return self.name
+
+
+def initializator(connections_amount=2, connection_propability=0.2):
+    """
+    initializator function
+    """
+    with open("items.yaml") as data_file:
+        items_data = safe_load(data_file)
+        items = [ Item(elem['name'], elem['description'], elem['capacity']) 
+                for elem in items_data ]
+
+    with open("rooms.yaml") as data_file:
+        rooms_data = safe_load(data_file)
+        rooms = [ Room(
+                    elem['name'],
+                    elem['description'],
+                    random.sample(items, random.randint(0,2)),
+                    bool(elem.get('water_source', 0))
+                    ) for elem in rooms_data ]
+
+    graph = graph_gen(len(rooms), connections_amount, connection_propability)
+    random.shuffle(rooms)
+    for no, room in enumerate(rooms):
+        room.id = no
+        room.neighbors = graph.neighbors(no)
+
+    for p in rooms:
+        print(p.id, p.name, p.neighbors, p.items)
+
+    fired_room = random.randint(1, len(rooms))
 
 
 FIRE = 1000
@@ -51,7 +68,8 @@ if __name__ == '__main__':
     room_order = list(range(room_number))
     random.shuffle(room_order)
     print(room_order)
-    graph = connected_watts_strogatz_graph(room_number+1, 2,0.2) #so called small world graph
+    #graph = connected_watts_strogatz_graph(room_number+1, 2,0.2) #so called small world graph
     print(rooms_data)
     print(Room(1, 3, 4).description)
     print(Item(1, 2).capacity)
+    initializator()
