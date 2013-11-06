53b6b81723a4b73305338d73fb83019a5554ed70
Mateusz Flieger <mateuszflieger@gmail.com>
2013-11-06 22:23:16 +0100

    Game class

--- server.py
+++ server.py
@@ -14,13 +14,28 @@ class Room:
         self.items = items
         self.water_source = water_source
         self.players = []
-        
+
+    def is_here(name):
+        return name in [player.name for player in self.players]
+
+
+class Player:
+    """
+    The player
+    """
+    def __init__(self, name, items = []):
+        self.name = name
+        self.items = items
+
+    def drop(self, index):
+        self.items.pop(index)
+
 
 class Item:
     """
     Item class
     """
-    def __init__(self, name, description, capacity=None):
+    def __init__(self, name, description, capacity=0):
         self.name = name
         self.description = description
         self.capacity = capacity
@@ -29,37 +44,44 @@ class Item:
         return self.name
 
 
-def initializator(connections_amount=2, connection_propability=0.2):
-    """
-    initializator function
-    """
-    with open("items.yaml") as data_file:
-        items_data = safe_load(data_file)
-        items = [ Item(elem['name'], elem['description'], elem['capacity']) 
-                for elem in items_data ]
 
-    with open("rooms.yaml") as data_file:
-        rooms_data = safe_load(data_file)
-        rooms = [ Room(
-                    elem['name'],
-                    elem['description'],
-                    random.sample(items, random.randint(0,2)),
-                    bool(elem.get('water_source', 0))
-                    ) for elem in rooms_data ]
+class Game:
+    def __init__(self, connections_amount=2, connection_propability=0.2):
+        self.fire = 1000
+        with open("items.yaml") as data_file:
+            items_data = safe_load(data_file)
+            self.items = [ Item(elem['name'], elem['description'], elem['capacity'])
+                          for elem in items_data ]
 
-    graph = graph_gen(len(rooms), connections_amount, connection_propability)
-    random.shuffle(rooms)
-    for no, room in enumerate(rooms):
-        room.id = no
-        room.neighbors = graph.neighbors(no)
+        with open("rooms.yaml") as data_file:
+            rooms_data = safe_load(data_file)
+            self.rooms = [ Room(
+                            elem['name'],
+                            elem['description'],
+                            random.sample(self.items, random.randint(0,2)),
+                            bool(elem.get('water_source', 0))
+                                ) for elem in rooms_data ]
 
-    for p in rooms:
-        print(p.id, p.name, p.neighbors, p.items)
+        self.graph = graph_gen(len(self.rooms), connections_amount,\
+                               connection_propability)
+        random.shuffle(self.rooms)
+        for no, room in enumerate(self.rooms):
+            room.id = no
+            room.neighbors = self.graph.neighbors(no)
 
-    fired_room = random.randint(1, len(rooms))
+        for p in self.rooms:
+            print(p.id, p.name, p.neighbors, p.items)
 
+        self.fired_room = random.randint(1, len(rooms))
 
-FIRE = 1000
+    def get_rooms():
+        return self.rooms
+
+    def where_is(name):
+        for room in self.rooms:
+            if room.is_here(name):
+                return room
+
 
 if __name__ == '__main__':
     with open("rooms.yaml") as data_file:
@@ -72,4 +94,5 @@ if __name__ == '__main__':
     print(rooms_data)
     print(Room(1, 3, 4).description)
     print(Item(1, 2).capacity)
-    initializator()
+    # initializator()
+    Game()
